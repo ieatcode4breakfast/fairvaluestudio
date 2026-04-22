bb676b8e9e3916c192c940693eca7f1e7abb1c16
Dwayne <dwayneletran17@gmail.com>
2026-04-22 16:34:40 +0800

    refactor: improve responsive chart layout and eliminate mobile gaps

--- src/components/scenario/GrowthProjectionChart.tsx
+++ src/components/scenario/GrowthProjectionChart.tsx
@@ -124,8 +124,8 @@ export function GrowthProjectionChart({ sc, results }: GrowthProjectionChartProp
       <div className="p-5 lg:p-6">
 
 
-        <div className="h-auto min-h-[320px]">
-          <ResponsiveContainer width="99%" aspect={2.1} debounce={50}>
+        <div className="h-[280px] sm:h-auto sm:aspect-[2.1/1]">
+          <ResponsiveContainer width="100%" height="100%" debounce={50}>
             <LineChart
               data={chartData}
               margin={{ top: 5, right: 10, left: 0, bottom: 5 }}
